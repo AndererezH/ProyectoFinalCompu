cd2fb38f0cdc0fc5405e1503fb1094e2ffd4a2af
AndererezH <92882601+AndererezH@users.noreply.github.com>
2021-10-20 14:49:28 -0500

    ProyectoFinal casi completo
    Creo que nada más le falta corregir un problema que tiene con el puntaje total, porque cuando sacas menos de 4 te pone el puntaje total en negativo. Ah, otra cosa, también falta poner todo en la función main() para que al final sólo la llamemos a esa función y se ejecute todo el código

--- ProyectoFinal.py
+++ ProyectoFinal.py
@@ -6,7 +6,10 @@ resp = ['b','d','b','b','a','a','c','b','c','b','a','c','c','b','c','b','c','d',
 
 repet = []
 
+contador = 0
+
 def opciones(n):
+    print('')
     print(data[n][0])
     print('a - ' + data[n][2])
     print('b - ' + data[n][3])
@@ -22,25 +25,98 @@ def mezclar(nivel):
         repet.append(x)
     return repet
 
+def respC(n):
+    if n == 'a':
+        x = 2
+    elif n == 'b':
+        x = 3
+    elif n == 'c':
+        x = 4
+    elif n == 'd':
+        x = 5
+    return x
+
+def level(niv):
+    contador = 0
+    repet = mezclar(niv)
+    for n in range(0,5):
+        opciones(repet[n])
+        ans = input('Respuesta: ')
+        if ans == resp[repet[n]]:
+            print('Respuesta correcta')
+            contador += 1
+        else:
+            print('Respuesta incorrecta')
+            opcionC = respC(resp[repet[n]])
+            print('Respuesta correcta: ' + resp[repet[n]] + ' - ' + data[repet[n]][opcionC])
+
+        if contador == 1:
+            print('Llevas ' + str(contador) + ' punto')
+        else:
+            print('Llevas ' + str(contador) + ' puntos')
+
+    return contador
+
+def plevel(niv):
+    if niv == 0:
+        nivel = 'Nivel fácil'
+    elif niv == 1:
+        nivel = 'Nivel medio'
+    elif niv == 2:
+        nivel = 'Nivel difícil'
+    return nivel
+
+def cambioNivel(buenas,nivel,puntuacion):
+    nivel = 0
+    if buenas >= 4 and nivel == 0:
+        print('Puntuación total: ' +  str(puntuacion))
+        print('Felicidades, avanzas al siguiente nivel')
+        nivel += 1
+        
+    elif buenas >= 4 and nivel == 1:
+        print('Puntuación total: ' +  str(puntuacion))
+        print('Felicidades, avanzas al siguiente nivel')
+        nivel += 1
+        
+    elif buenas >= 4 and nivel == 2:
+        print('Puntuación total: ' +  str(puntuacion))
+        print('Felcidades, has completado con éxito los 3 niveles')
+    return nivel
+
 file = open('PreguntasCompu.csv')
 
 readfile = csv.reader(file)
 
 data = list(readfile)
 
-nivel = 'fácil'
-
-print('Nivel ' + nivel)
+nivel = 0
 
 while True:
-    if nivel == 'fácil':
-        niv = 0
-        repet = mezclar(niv)
-        for n in range(0,5):
-            print(repet)
-            opciones(repet[0])
-            ans = input('Respuesta: ')
-            if ans == resp[repet[0]]:
-               print('bien') 
-            
-        break
+
+    print('')
+    print(plevel(nivel))
+
+    buenas = level(nivel)
+
+    puntuacion = contador + buenas
+
+    if buenas < 4 and nivel != 0:
+        puntuacion = contador - buenas
+        print('Puntuación total: ' +  str(puntuacion))
+        repetir = input("¿Quieres volver a jugar?(si o no): ")
+        repetir.lower()
+        if repetir == 'si':
+            continue
+        elif repetir == 'no':
+            break
+
+    elif buenas < 4:
+        print('Puntuación total: ' +  str(puntuacion))
+        repetir = input("¿Quieres volver a jugar?(si o no): ")
+        repetir.lower()
+        if repetir == 'si':
+            continue
+        elif repetir == 'no':
+            break
+
+    nivel += cambioNivel(buenas,nivel,puntuacion)
